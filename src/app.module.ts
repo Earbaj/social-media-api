991f8feecf137b35d767ca5e0ccbcddadc16039b
Earbaj <earbajsaria3@gmail.com>
2026-04-06 12:53:44 +0600

    add: nest config package

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot("")],
+  imports: [UsersModule, // ১. ConfigModule লোড করা
+    ConfigModule.forRoot({
+      isGlobal: true, // যাতে সব মডিউলে বারবার ইমপোর্ট করতে না হয়
+    }),
+
+    // ২. Async ভাবে Mongoose কানেক্ট করা
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
