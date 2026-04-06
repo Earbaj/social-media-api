1481d4a73c9a7967980273802f17fd6cf4f5ef6b
Earbaj <earbajsaria3@gmail.com>
2026-04-06 12:41:55 +0600

    ADD:functionality to user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { User } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async followUser(currentUserId: string, targetUserId: string) {
+    // Current user-er following list-e target ke add kora
+    await this.userModel.findByIdAndUpdate(currentUserId, {
+      $addToSet: { following: new Types.ObjectId(targetUserId) }
+    });
+    // Target user-er follower list-e current user ke add kora
+    await this.userModel.findByIdAndUpdate(targetUserId, {
+      $addToSet: { followers: new Types.ObjectId(currentUserId) }
+    });
+    return { message: 'Followed successfully' };
+  }
+
+  async getProfile(username: string) {
+    return this.userModel.findOne({ username }).populate('followers following', 'username profilePic');
+  }
+}
