6231f32f7cf50e44eab47ef6bdf4a89e80e09e8d
Earbaj <earbajsaria3@gmail.com>
2026-04-06 13:52:11 +0600

    add: posts module , controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [UsersModule, // ১. ConfigModule লোড করা
@@ -21,7 +22,9 @@ import { AuthModule } from './auth/auth.module';
       }),
     }),
 
-    AuthModule,],
+    AuthModule,
+
+    PostsModule,],
   controllers: [AppController],
   providers: [AppService],
 })
